da3f7df7194a71a8e2c73237b28dfa6aae368d38
Anastasiia_Bl <blinova-nastia@ukr.net>
2024-06-09 01:01:06 +0300

    UPDATE: change roots for images src for correct deploy

--- src/components/Works/worksImg.js
+++ src/components/Works/worksImg.js
@@ -1,46 +1,46 @@
 const worksImg = [
     {
-      imgSrc: "../../../public/img/works/1.webp",
+      imgSrc: "/img/works/1.webp",
       title: "Глянцеві натяжні стелі",
     },
     {
-      imgSrc: "../../../public/img/works/2.webp",
+      imgSrc: "/img/works/2.webp",
       title: "Глянцеві натяжні стелі",
     },
     {
-      imgSrc: "../../../public/img/works/3.webp",
+      imgSrc: "/img/works/3.webp",
       title: "Глянцеві натяжні стелі",
     },
     {
-      imgSrc: "../../../public/img/works/4.webp",
+      imgSrc: "/img/works/4.webp",
       title: "Глянцеві натяжні стелі",
     },
     {
-      imgSrc: "../../../public/img/works/5.webp",
+      imgSrc: "/img/works/5.webp",
       title: "Глянцеві натяжні стелі",
     },
     {
-      imgSrc: "../../../public/img/works/6.webp",
+      imgSrc: "/img/works/6.webp",
       title: "Глянцеві натяжні стелі",
     },
     {
-      imgSrc: "../../../public/img/works/7.webp",
+      imgSrc: "/img/works/7.webp",
       title: "Глянцеві натяжні стелі",
     },
     {
-      imgSrc: "../../../public/img/works/8.webp",
+      imgSrc: "/img/works/8.webp",
       title: "Глянцеві натяжні стелі",
     },
     {
-      imgSrc: "../../../public/img/works/9.webp",
+      imgSrc: "/img/works/9.webp",
       title: "Глянцеві натяжні стелі",
     },
     {
-      imgSrc: "../../../public/img/works/10.webp",
+      imgSrc: "/img/works/10.webp",
       title: "Глянцеві натяжні стелі",
     },
     {
-      imgSrc: "../../../public/img/works/12.webp",
+      imgSrc: "/img/works/12.webp",
       title: "Глянцеві натяжні стелі",
     },
   ];
